7bc0a2a811951d2549d74b78488b4e2168bd17bd
tsgreenberg1217 <tsgreenberg1217@gmail.com>
2017-12-04 10:17:40 -0500

    added dynamic forms-now making submit function

--- src/components/addressBar.js
+++ src/components/addressBar.js
@@ -1,19 +1,53 @@
 import React from 'react'
 
 
-const AddressBar = (props) =>{
-  return(
-  <form onSubmit= {props.handleSubmit}>
-    <input
-    type = "text"
-    value = {props.value}
-    onChange = {props.handleChange}
-    />
-    <br/>
-    <button type = 'submit'> Submit</button>
-    <br/>
-  </form>
-)
+
+class AddressBar extends React.Component{
+  constructor(){
+    super()
+    this.state= {
+      addresses: [{address: ''},{address: ''}]
+    }
+    this.handleAddressChange = this.handleAddressChange.bind(this)
+    this.addAddress = this.addAddress.bind(this)
+  }
+
+  handleAddressChange = (name, index) => {
+    // make a copy of proportions object
+    this.setState(prevState => {
+      return {
+        addresses: [
+          ...prevState.addresses.slice(0, index),
+          { address: name },
+          ...prevState.addresses.slice(index + 1)
+        ]
+      };
+    });
+  };
+
+  addAddress(){
+    this.setState({
+      addresses: [...this.state.addresses, {address: ''}]
+    })
+  }
+
+  render(){
+    return(
+      <form onSubmit= {this.props.handleSubmit}>
+      {this.state.addresses.map((address,i) =>
+       (<input
+        type = "text"
+        onChange = {e => this.handleAddressChange(e.target.value, i)}
+        />)
+      )}
+      <br/>
+      <button onClick = {this.addAddress}>add address</button>
+      <button type = 'submit'> Submit</button>
+      <br/>
+      </form>
+    )
+
+  }
 }
 
 export default AddressBar
