1f97227006ffee30d108775008609d4233b76e59
tsgreenberg1217 <tsgreenberg1217@gmail.com>
2017-12-04 14:14:53 -0500

    pushing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import MapContainer from './components/map'
 
-
 class App extends React.Component {
   render() {
     return (
       <div>
-      
       <MapContainer/>
       </div>
     );
